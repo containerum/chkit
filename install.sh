a0834ac97d9dc4d8096c040932de21708f1f8258
sfilatov96 <stepa-filatov@yandex.ru>
2017-04-27 16:11:50 +0300

    added update-file

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ sudo pip3 install -r requirements.txt
 sudo mkdir -p /var/lib/containerium/src
 sudo cp -r ./*  /ver/lib/containerium/src/
 sudo chmod +x client.py
-sudo ln -srf /ver/lib/containerium/src/client.py /usr/bin/client
+sudo ln -srf /var/lib/containerium/src/client.py /usr/bin/client
 sudo cp /var/lib/containerium/src/CONFIG.json /var/lib/containerium/CONFIG.json
