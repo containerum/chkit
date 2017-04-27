fbb476ce8e867caef88bd5fbab839a38964663c0
sfilatov96 <stepa-filatov@yandex.ru>
2017-04-27 18:34:06 +0300

    creating folder .containerium

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo pip3 install -r requirements.txt
-sudo mkdir -p /var/lib/containerium/src
-sudo cp -r ./*  /var/lib/containerium/src/
+sudo mkdir -p $HOME/.containerium/src
+sudo cp -r ./*  $HOME/.containerium/src/
 sudo chmod +x client.py
-sudo ln -srf /var/lib/containerium/src/client.py /usr/bin/client
-sudo cp /var/lib/containerium/src/CONFIG.json /var/lib/containerium/CONFIG.json
-sudo chmod 777 /var/lib/containerium/CONFIG.json
-sudo chmod 777 /var/lib/containerium/src/json_templates/run.json
+sudo ln -srf $HOME/.containerium/src/client.py /usr/bin/client
+sudo cp $HOME/.containerium/src/CONFIG.json $HOME/.containerium/CONFIG.json
+sudo chmod 777 $HOME/.containerium/CONFIG.json
+sudo chmod 777 $HOME/.containerium/src/json_templates/run.json
