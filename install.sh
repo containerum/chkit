9616f1500ce39acb59d0972d7ec94bdb378bfc14
sfilatov96 <stepa-filatov@yandex.ru>
2017-04-27 16:20:04 +0300

    repair path/to/files

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo pip3 install -r requirements.txt
 sudo mkdir -p /var/lib/containerium/src
-sudo cp -r ./*  /ver/lib/containerium/src/
+sudo cp -r ./*  /var/lib/containerium/src/
 sudo chmod +x client.py
 sudo ln -srf /var/lib/containerium/src/client.py /usr/bin/client
 sudo cp /var/lib/containerium/src/CONFIG.json /var/lib/containerium/CONFIG.json
