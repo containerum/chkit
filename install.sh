e22d8dd5c7c0cb9afb5112dfc03531f241da15b7
sfilatov96 <stepa-filatov@yandex.ru>
2017-04-27 16:33:53 +0300

    777 for config.json

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ sudo cp -r ./*  /var/lib/containerium/src/
 sudo chmod +x client.py
 sudo ln -srf /var/lib/containerium/src/client.py /usr/bin/client
 sudo cp /var/lib/containerium/src/CONFIG.json /var/lib/containerium/CONFIG.json
+sudo chmod 777 /var/lib/containerium/CONFIG.json
