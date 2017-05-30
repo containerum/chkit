2af53c36a09e91c5ade6a1ac403e3adb353b1b99
sfilatov96 <stepa-filatov@yandex.ru>
2017-05-30 19:32:03 +0300

    chmod fixed

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,4 @@
 mkdir -p $HOME/.containerum/src/json_templates
 DIRECTORY=$(cd install $0 && pwd)
 cp $DIRECTORY/CONFIG.json $HOME/.containerum/CONFIG.json
-chmod $HOME/.containerum/ -R
+chmod 777 -R $HOME/.containerum/
