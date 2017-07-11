3ece46a1e47c5a3d9a978c45a636aeb487077f12
Ivan <ivan@MacBook-Pro-Konstantin.local>
2017-07-11 14:00:30 +0300

    add debug flag

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,28 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+	jww "github.com/spf13/jwalterweatherman"
+)
 
 //RootCmd main cmd entrypoint
 var RootCmd = &cobra.Command{
 	Use: "chkit",
+	Run: func(cmd *cobra.Command, args []string) {
+		if cmd.Flags().NFlag() == 0 {
+			cmd.Usage()
+		}
+		var err error
+		debug, err = cmd.Flags().GetBool("debug")
+		if err != nil {
+			jww.ERROR.Println(err)
+			return
+		}
+	},
+}
+
+var debug bool
+
+func init() {
+	RootCmd.PersistentFlags().BoolP("debug", "d", false, "turn on debugging messages")
 }
