851294872aa40a76079335e11d4d82dd9439f2bb
Ivan <ivan@MacBook-Pro-Konstantin.local>
2017-07-17 10:05:15 +0300

    fix namespace print

--- chlib/getFuncs.go
+++ chlib/getFuncs.go
@@ -21,30 +21,13 @@ func GetCmdRequestJson(client *Client, kind, name string) (ret []GenericJson, er
 			err = fmt.Errorf("can`t extract field: %s", r)
 		}
 	}()
-	var apiResult TcpApiResult
-	switch kind {
-	case KindNamespaces:
-		apiResult, err = client.Get(KindNamespaces, name, "")
-		if err != nil {
-			return ret, err
-		}
-		items := apiResult["results"].([]interface{})
-		for _, itemI := range items {
-			item := itemI.(map[string]interface{})
-			_, hasNs := item["data"].(map[string]interface{})["metadata"].(map[string]interface{})["namespace"]
-			if hasNs {
-				ret = append(ret, GenericJson(item))
-			}
-		}
-	default:
-		apiResult, err := client.Get(kind, name, client.userConfig.Namespace)
-		if err != nil {
-			return ret, err
-		}
-		items := apiResult["results"].([]interface{})
-		for _, itemI := range items {
-			ret = append(ret, itemI.(map[string]interface{}))
-		}
+	apiResult, err := client.Get(kind, name, client.userConfig.Namespace)
+	if err != nil {
+		return ret, err
+	}
+	items := apiResult["results"].([]interface{})
+	for _, itemI := range items {
+		ret = append(ret, itemI.(map[string]interface{}))
 	}
 	return
 }
