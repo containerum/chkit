b035e16fda47ec77f89bdc2697a923471ca8b665
paul <petrukhin.paul@gmail.com>
2018-02-20 16:02:46 +0300

    extract save token method

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,13 +1,37 @@
 package client
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+
 	kubeClient "git.containerum.net/ch/kube-client/pkg/cmd"
 	kubeModels "git.containerum.net/ch/kube-client/pkg/model"
+	"github.com/containerum/chkit/pkg/err"
 	"github.com/containerum/chkit/pkg/model"
+)
+
+const (
+	ErrUnableToPackTokens    = err.Err("unable to pack tokens")
+	ErrUnableToSaveTokenFile = err.Err("unable to save token file")
 )
 
 type ChkitClient struct {
 	kube   kubeClient.Client
 	tokens kubeModels.Tokens
 	config model.Config
+}
+
+func (client *ChkitClient) SaveTokens() error {
+	packedTokens, err := json.Marshal(client.tokens)
+	if err != nil {
+		return ErrUnableToPackTokens.Wrap(err)
+	}
+	err = ioutil.WriteFile(client.config.TokenFile,
+		packedTokens,
+		os.ModePerm)
+	if err != nil {
+		return ErrUnableToSaveTokenFile.Wrap(err)
+	}
+	return nil
 }
--- pkg/client/login.go
+++ pkg/client/login.go
@@ -1,18 +1,12 @@
 package client
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"os"
-
 	kubeModels "git.containerum.net/ch/kube-client/pkg/model"
 	"github.com/containerum/chkit/pkg/err"
 )
 
-var (
-	ErrUnableToLogin         = err.Err("unable to login")
-	ErrUnableToPackTokens    = err.Err("unable to pack tokens")
-	ErrUnableToSaveTokenFile = err.Err("unable to save token file")
+const (
+	ErrUnableToLogin = err.Err("unable to login")
 )
 
 func (client *ChkitClient) Login(username, password string) error {
@@ -24,12 +18,5 @@ func (client *ChkitClient) Login(username, password string) error {
 		return ErrUnableToLogin.Wrap(err)
 	}
 	client.tokens = tokens
-	packedTokens, err := json.Marshal(tokens)
-	if err != nil {
-		return ErrUnableToPackTokens.Wrap(err)
-	}
-	err = ioutil.WriteFile(client.config.TokenFile,
-		packedTokens,
-		os.ModePerm)
-	return err
+	return nil
 }
