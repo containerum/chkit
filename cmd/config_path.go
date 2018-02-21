a320b35c8a3bf6a38b081e7968d32c861236fce3
paul <petrukhin.paul@gmail.com>
2018-02-21 11:12:03 +0300

    refactor configuration store

--- cmd/config_path.go
+++ cmd/config_path.go
@@ -6,14 +6,10 @@ import (
 	homedir "github.com/mitchellh/go-homedir"
 )
 
-var (
-	ConfigPath string
-)
-
 func init() {
 	home, err := homedir.Dir()
 	if err != nil {
 		notepad.FATAL.Println(err)
 	}
-	ConfigPath = path.Join(home, configDir)
+	Configuration.ConfigPath = path.Join(home, configDir)
 }
