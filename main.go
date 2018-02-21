05efba43dfbcaf89a425423ab0fc6114aabf59d9
paul <petrukhin.paul@gmail.com>
2018-02-21 11:11:49 +0300

    fix main func

--- main.go
+++ main.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/containerum/chkit/chlib"
 	"github.com/containerum/chkit/cmd"
 )
 
 func main() {
-	if _, err := os.Stat(chlib.ConfigDir); os.IsNotExist(err) {
-		os.MkdirAll(chlib.ConfigDir, os.ModePerm)
-		os.MkdirAll(chlib.TemplatesDir, os.ModePerm)
-		os.MkdirAll(chlib.SrcFolder, os.ModePerm)
-	}
-	if err := cmd.RootCmd.Execute(); err != nil {
+	if err := cmd.App.Execute(); err != nil {
 		fmt.Println(err)
+		os.Exit(1)
+	}
+	if _, err := os.Stat(cmd.Configuration.ConfigPath); !os.IsNotExist(err) {
+		return
+	}
+	if err := os.MkdirAll(cmd.Configuration.ConfigPath, os.ModePerm); err != nil {
+		fmt.Printf("error while creating config dir: %v", err)
 		os.Exit(1)
 	}
 }
