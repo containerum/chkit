dc02f18da524271385e4d34ced1c265895199f1f
paul <petrukhin.paul@gmail.com>
2018-02-26 16:44:10 +0300

    refactor cli

--- cmd/init.go
+++ cmd/init.go
@@ -1,16 +1,10 @@
 package cmd
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-
 	"github.com/containerum/chkit/pkg/client"
 	"github.com/containerum/chkit/pkg/model"
-	"github.com/spf13/cobra"
 	jww "github.com/spf13/jwalterweatherman"
-	"github.com/spf13/viper"
+	cli "gopkg.in/urfave/cli.v2"
 )
 
 const (
@@ -27,45 +21,10 @@ var Configuration = struct {
 
 var ChkitClient client.ChkitClient
 var notepad *jww.Notepad
-var App = &cobra.Command{
-	Use: "chkit",
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		if debug, _ := cmd.Flags().GetBool("debug"); debug {
-			notepad = jww.NewNotepad(jww.LevelDebug,
-				jww.LevelDebug,
-				os.Stdout,
-				ioutil.Discard,
-				"", log.Ldate|log.Ltime)
-		} else {
-			notepad = jww.NewNotepad(jww.LevelInfo,
-				jww.LevelInfo,
-				os.Stdout,
-				ioutil.Discard,
-				"", log.Ldate|log.Ltime)
-		}
 
+var App = &cli.App{
+	Name: "chkit",
+	Action: func(ctx *cli.Context) error {
+		return nil
 	},
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	App.PersistentFlags().StringVar(&Configuration.ConfigFile,
-		FlagConfigFile, "", "config file (default is "+Configuration.ConfigPath+"/containerum.yaml)")
-	viper.BindPFlag(FlagConfigFile, App.PersistentFlags().Lookup(FlagConfigFile))
-
-	App.PersistentFlags().StringVar(&Configuration.ClientConfig.APIaddr,
-		FlagAPIaddr, "", "API address")
-
-}
-func initConfig() {
-	viper.AddConfigPath(Configuration.ConfigPath)
-	viper.SetConfigName("containerum")
-	viper.AutomaticEnv()
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
--- cmd/login.go
+++ cmd/login.go
@@ -6,43 +6,20 @@ import (
 	"strings"
 	"syscall"
 
-	"github.com/spf13/cobra"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-var loginCmd = &cobra.Command{
-	Use:   "login",
-	Short: "Open session and set up token",
-	Run: func(cmd *cobra.Command, args []string) {
-		var email string
-		if !cmd.Flag("login").Changed {
-			notepad.FEEDBACK.Print("Enter your email: ")
-			var err error
-			email, err = bufio.NewReader(os.Stdin).ReadString('\n')
-			email = strings.TrimRight(email, "\r\n")
-			exitOnErr(err)
-		} else {
-			email = cmd.Flag("login").Value.String()
-		}
-		var password string
-		if !cmd.Flag("password").Changed {
-			notepad.FEEDBACK.Print("Enter your password: ")
-			passwordB, err := terminal.ReadPassword(int(syscall.Stdin))
-			exitOnErr(err)
-			password = string(passwordB)
-		} else {
-			password = cmd.Flag("password").Value.String()
-		}
-		exitOnErr(ChkitClient.Login(email, password))
-		notepad.FEEDBACK.Println("Succesfull login!")
-		file, err := os.Create(Configuration.TokenFile)
-		exitOnErr(err)
-		defer file.Close()
-		exitOnErr(ChkitClient.SaveTokens(file))
-	},
+func readLogin() string {
+	notepad.FEEDBACK.Print("Enter your email: ")
+	email, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	email = strings.TrimRight(email, "\r\n")
+	exitOnErr(err)
+	return email
 }
 
-func init() {
-	loginCmd.PersistentFlags().StringP("login", "l", "", "User login (email)")
-	loginCmd.PersistentFlags().StringP("password", "p", "", "User password")
+func readPassword() string {
+	notepad.FEEDBACK.Print("Enter your password: ")
+	passwordB, err := terminal.ReadPassword(int(syscall.Stdin))
+	exitOnErr(err)
+	return string(passwordB)
 }
