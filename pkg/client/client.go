46f7b68b1ebf2848d401b2d7796c99cdea2dff11
paul <petrukhin.paul@gmail.com>
2018-03-01 12:37:20 +0300

    add client

---
+++ pkg/client/client.go
@@ -0,0 +1,33 @@
+package client
+
+import (
+	kubeClient "git.containerum.net/ch/kube-client/pkg/client"
+	kubeClientModels "git.containerum.net/ch/kube-client/pkg/model"
+	"github.com/containerum/chkit/pkg/chkitErrors"
+	"github.com/containerum/chkit/pkg/model"
+)
+
+type Client struct {
+	tokens        kubeClientModels.Tokens
+	kubeApiClient kubeClient.Client
+}
+
+func NewClient(config model.Config) (*Client, error) {
+	chcli := &Client{}
+	kubecli, err := kubeClient.CreateCmdClient(kubeClient.Config{
+		APIurl:         config.APIaddr + ":1214",
+		UserManagerURL: config.APIaddr + ":8111",
+		ResourceAddr:   config.APIaddr + ":1213",
+		AuthURL:        config.APIaddr + ":1111",
+		User: kubeClient.User{
+			Role: "user",
+		},
+	})
+	if err != nil {
+		err = chkitErrors.ErrUnableToInitClient().
+			AddDetailsErr(err)
+		return nil, err
+	}
+	chcli.kubeApiClient = *kubecli
+	return chcli, nil
+}
