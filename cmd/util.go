6eb87e87e343e706533d56c0c386570092480f6b
paul <petrukhin.paul@gmail.com>
2018-03-01 15:43:52 +0300

    add tokens path

--- cmd/util.go
+++ cmd/util.go
@@ -1,13 +1,14 @@
 package cmd
 
 import (
+	"encoding/json"
 	"os"
 	"path"
 
-	"github.com/sirupsen/logrus"
-
+	kubeClientModels "git.containerum.net/ch/kube-client/pkg/model"
 	"github.com/BurntSushi/toml"
 	"github.com/containerum/chkit/pkg/model"
+	"github.com/sirupsen/logrus"
 )
 
 func exitOnErr(log *logrus.Logger, err error) {
@@ -35,4 +36,21 @@ func saveConfig(config *model.Config) error {
 		return err
 	}
 	return toml.NewEncoder(file).Encode(&config.Client)
+}
+
+func saveTokens(config *model.Config) error {
+	file, err := os.Create(path.Join(config.ConfigPath, "tokens"))
+	if err != nil {
+		return err
+	}
+	return json.NewEncoder(file).Encode(&config.Tokens)
+}
+
+func loadTokens(config *model.Config) (kubeClientModels.Tokens, error) {
+	tokens := kubeClientModels.Tokens{}
+	file, err := os.Open(path.Join(config.ConfigPath, "tokens"))
+	if err != nil {
+		return tokens, err
+	}
+	return tokens, json.NewDecoder(file).Decode(&tokens)
 }
