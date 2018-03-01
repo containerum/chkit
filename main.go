25827a1c4f47c5763ca33a12ec0abbf75f7d8332
paul <petrukhin.paul@gmail.com>
2018-03-01 12:56:51 +0300

    refactor main start point

--- main.go
+++ main.go
@@ -8,15 +8,7 @@ import (
 )
 
 func main() {
-	if err := cmd.App.Execute(); err != nil {
+	if err := cmd.App.Run(os.Args); err != nil {
 		fmt.Println(err)
-		os.Exit(1)
-	}
-	if _, err := os.Stat(cmd.Configuration.ConfigPath); !os.IsNotExist(err) {
-		return
-	}
-	if err := os.MkdirAll(cmd.Configuration.ConfigPath, os.ModePerm); err != nil {
-		fmt.Printf("error while creating config dir: %v", err)
-		os.Exit(1)
 	}
 }
