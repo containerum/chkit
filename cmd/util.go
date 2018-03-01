f85f16af0554fef930fedc1188d5ecd8c7093637
paul <petrukhin.paul@gmail.com>
2018-03-01 13:50:16 +0300

    add loadConfig func

--- cmd/util.go
+++ cmd/util.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"os"
+
+	"github.com/BurntSushi/toml"
+	"github.com/containerum/chkit/pkg/model"
 )
 
 func exitOnErr(err error) {
@@ -9,4 +12,14 @@ func exitOnErr(err error) {
 		log.WithError(err).Fatal(err)
 		os.Exit(1)
 	}
+}
+
+func loadConfig(configFilePath string) error {
+	config := model.Config{}
+	_, err := toml.DecodeFile(configFilePath, &config)
+	if err != nil {
+		return err
+	}
+	Configuration = config
+	return nil
 }
