034b92edcbd4d26e807ab4f1f4effc2e10d1f042
paul <petrukhin.paul@gmail.com>
2018-03-02 10:50:17 +0300

    use context storage

--- cmd/login.go
+++ cmd/login.go
@@ -9,32 +9,33 @@ import (
 
 	"github.com/sirupsen/logrus"
 
-	"github.com/containerum/chkit/pkg/model"
 	"golang.org/x/crypto/ssh/terminal"
 	cli "gopkg.in/urfave/cli.v2"
 )
 
-func commandLogin(log *logrus.Logger, config *model.Config) *cli.Command {
+func commandLogin(log *logrus.Logger, configPath string) *cli.Command {
 	return &cli.Command{
 		Name:  "login",
 		Usage: "use username and password to login in the system",
 		Action: func(ctx *cli.Context) error {
+			chClient := getClient(ctx)
 			if ctx.IsSet("username") {
-				config.Client.Username = ctx.String("username")
+				chClient.Config.Username = ctx.String("username")
 			} else {
-				config.Client.Username = readLogin(log)
+				chClient.Config.Username = readLogin(log)
 			}
 			if ctx.IsSet("pass") {
-				config.Client.Password = ctx.String("pass")
+				chClient.Config.Password = ctx.String("pass")
 			} else {
-				config.Client.Password = readPassword(log)
+				chClient.Config.Password = readPassword(log)
 			}
-			err := saveConfig(config)
+			err := saveConfig(configPath, &chClient.Config)
 			if err != nil {
 				log.WithError(err).
 					Errorf("error while saving config file")
 				return err
 			}
+			setClient(ctx, chClient)
 			return nil
 		},
 		Flags: []cli.Flag{
