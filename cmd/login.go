bfda76265d956db3796e5bfe7f69267f9e8c8045
paul <petrukhin.paul@gmail.com>
2018-03-02 11:58:41 +0300

    refactor commanfLogin

--- cmd/login.go
+++ cmd/login.go
@@ -13,37 +13,36 @@ import (
 	cli "gopkg.in/urfave/cli.v2"
 )
 
-func commandLogin(configPath string) *cli.Command {
-	return &cli.Command{
-		Name:  "login",
-		Usage: "use username and password to login in the system",
-		Action: func(ctx *cli.Context) error {
-			log := getLog(ctx)
-			chClient := getClient(ctx)
-			if ctx.IsSet("username") {
-				chClient.Config.Username = ctx.String("username")
-			} else {
-				chClient.Config.Username = readLogin(log)
-			}
-			if ctx.IsSet("pass") {
-				chClient.Config.Password = ctx.String("pass")
-			} else {
-				chClient.Config.Password = readPassword(log)
-			}
-			setClient(ctx, chClient)
-			return nil
+var commandLogin = &cli.Command{
+	Name:  "login",
+	Usage: "use username and password to login in the system",
+	Action: func(ctx *cli.Context) error {
+		log := getLog(ctx)
+		chClient := getClient(ctx)
+		if ctx.IsSet("username") {
+			chClient.Config.Username = ctx.String("username")
+		} else {
+			chClient.Config.Username = readLogin(log)
+		}
+		if ctx.IsSet("pass") {
+			chClient.Config.Password = ctx.String("pass")
+		} else {
+			chClient.Config.Password = readPassword(log)
+		}
+		setClient(ctx, chClient)
+		saveConfig(ctx, chClient.Config)
+		return nil
+	},
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "pass",
+			Usage: "password to system",
 		},
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:  "pass",
-				Usage: "password to system",
-			},
-			&cli.StringFlag{
-				Name:  "username",
-				Usage: "your account email",
-			},
+		&cli.StringFlag{
+			Name:  "username",
+			Usage: "your account email",
 		},
-	}
+	},
 }
 
 func readLogin(log *logrus.Logger) string {
