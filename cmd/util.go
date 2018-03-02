7c5ff7d6afa0aad8cd8a5e74fa37b141d22cc0f2
paul <petrukhin.paul@gmail.com>
2018-03-02 10:49:18 +0300

    add get and set client funcs

--- cmd/util.go
+++ cmd/util.go
@@ -7,10 +7,19 @@ import (
 
 	kubeClientModels "git.containerum.net/ch/kube-client/pkg/model"
 	"github.com/BurntSushi/toml"
+	"github.com/containerum/chkit/pkg/client"
 	"github.com/containerum/chkit/pkg/model"
 	"github.com/sirupsen/logrus"
+	"gopkg.in/urfave/cli.v2"
 )
 
+func getClient(ctx *cli.Context) client.Client {
+	return ctx.App.Metadata["client"].(client.Client)
+}
+
+func setClient(ctx *cli.Context, chClient client.Client) {
+	ctx.App.Metadata["client"] = chClient
+}
 func exitOnErr(log *logrus.Logger, err error) {
 	if err != nil {
 		log.WithError(err).Fatal(err)
@@ -18,7 +27,7 @@ func exitOnErr(log *logrus.Logger, err error) {
 	}
 }
 
-func loadConfig(config *model.ClientConfig, configFilePath string) error {
+func loadConfig(configFilePath string, config *model.ClientConfig) error {
 	_, err := toml.DecodeFile(configFilePath, &config)
 	if err != nil {
 		return err
@@ -26,16 +35,16 @@ func loadConfig(config *model.ClientConfig, configFilePath string) error {
 	return nil
 }
 
-func saveConfig(config *model.Config) error {
-	err := os.MkdirAll(config.ConfigPath, os.ModePerm)
+func saveConfig(configPath string, config *model.ClientConfig) error {
+	err := os.MkdirAll(configPath, os.ModePerm)
 	if err != nil && !os.IsExist(err) {
 		return err
 	}
-	file, err := os.Create(path.Join(config.ConfigPath, "config.toml"))
+	file, err := os.Create(path.Join(configPath, "config.toml"))
 	if err != nil {
 		return err
 	}
-	return toml.NewEncoder(file).Encode(&config.Client)
+	return toml.NewEncoder(file).Encode(&config)
 }
 
 func saveTokens(config *model.Config) error {
