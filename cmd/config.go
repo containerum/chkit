2e4e1494e4dbab8e54ffc5417ce74331cf1e06f2
paul <petrukhin.paul@gmail.com>
2018-03-02 10:49:39 +0300

    refactor config path getter

--- cmd/config.go
+++ cmd/config.go
@@ -3,15 +3,13 @@ package cmd
 import (
 	"path"
 
-	"github.com/containerum/chkit/pkg/model"
 	homedir "github.com/mitchellh/go-homedir"
 )
 
-func initConfig(config *model.Config) error {
+func getConfigPath() (string, error) {
 	home, err := homedir.Dir()
 	if err != nil {
-		return err
+		return "", err
 	}
-	config.ConfigPath = path.Join(home, configDir)
-	return nil
+	return path.Join(home, configDir), nil
 }
