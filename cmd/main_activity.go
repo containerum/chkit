e96f4cd8bf3f9313a992b5842bb5cadf988781bb
paul <petrukhin.paul@gmail.com>
2018-03-05 10:39:33 +0300

    extend main activity

--- cmd/main_activity.go
+++ cmd/main_activity.go
@@ -1,13 +1,18 @@
 package cmd
 
 import (
+	"fmt"
+
 	"gopkg.in/urfave/cli.v2"
 )
 
 func mainActivity(ctx *cli.Context) error {
+	fmt.Println("main activity")
 	client := getClient(ctx)
 	if err := client.Login(); err != nil {
 		return err
 	}
+	setTokens(ctx, client.Tokens)
+	saveTokens(ctx, client.Tokens)
 	return nil
 }
