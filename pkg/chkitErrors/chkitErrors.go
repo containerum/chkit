05843c54030ff990004a63dfc83c8d8dcefe60c8
paul <petrukhin.paul@gmail.com>
2018-03-05 10:39:03 +0300

    add errors

--- pkg/chkitErrors/chkitErrors.go
+++ pkg/chkitErrors/chkitErrors.go
@@ -2,15 +2,38 @@ package chkitErrors
 
 import (
 	bytes "bytes"
-	template "text/template"
-
 	cherry "git.containerum.net/ch/kube-client/pkg/cherry"
+	template "text/template"
 )
 
 const ()
 
 func ErrUnableToInitClient(params ...func(*cherry.Err)) *cherry.Err {
 	err := &cherry.Err{Message: "unable to init client", StatusHTTP: 418, ID: cherry.ErrID{SID: 0x309, Kind: 0x1}, Details: []string(nil)}
+	for _, param := range params {
+		param(err)
+	}
+	for i, detail := range err.Details {
+		det := renderTemplate(detail)
+		err.Details[i] = det
+	}
+	return err
+}
+
+func ErrInvalidUsername(params ...func(*cherry.Err)) *cherry.Err {
+	err := &cherry.Err{Message: "invalid username", StatusHTTP: 418, ID: cherry.ErrID{SID: 0x309, Kind: 0x2}, Details: []string(nil)}
+	for _, param := range params {
+		param(err)
+	}
+	for i, detail := range err.Details {
+		det := renderTemplate(detail)
+		err.Details[i] = det
+	}
+	return err
+}
+
+func ErrInvalidPassword(params ...func(*cherry.Err)) *cherry.Err {
+	err := &cherry.Err{Message: "invalid password", StatusHTTP: 418, ID: cherry.ErrID{SID: 0x309, Kind: 0x3}, Details: []string(nil)}
 	for _, param := range params {
 		param(err)
 	}
