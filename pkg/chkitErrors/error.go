0cab67b5dea589dc4f905905c055e74384c2aba0
paul <petrukhin.paul@gmail.com>
2018-03-12 11:31:00 +0300

    add cli interface impls

--- pkg/chkitErrors/error.go
+++ pkg/chkitErrors/error.go
@@ -3,22 +3,47 @@ package chkitErrors
 import (
 	"bytes"
 	"fmt"
+
+	"gopkg.in/urfave/cli.v2"
 )
 
 type Err string
 
+var (
+	_ error          = Err("")
+	_ cli.ExitCoder  = Err("")
+	_ cli.MultiError = Err("")
+
+	_ error          = &Wrapper{}
+	_ cli.ExitCoder  = &Wrapper{}
+	_ cli.MultiError = &Wrapper{}
+)
+
 func (err Err) Error() string {
 	return string(err)
 }
-
 func (err Err) Errors() []error {
 	return []error{err}
+}
+
+func (err Err) ExitCode() int {
+	return 1
+}
+func (err Err) Wrap(errs ...error) *Wrapper {
+	return Wrap(err, errs...)
 }
 
 type Wrapper struct {
 	main          error
 	reasons       []error
 	cachedMessage string
+}
+
+func Wrap(err error, reasons ...error) *Wrapper {
+	return &Wrapper{
+		main:    err,
+		reasons: reasons,
+	}
 }
 
 func (wrapper *Wrapper) AddReasons(reasons ...error) *Wrapper {
@@ -63,4 +88,8 @@ func (wrapper *Wrapper) Errors() []error {
 	errs := make([]error, len(wrapper.reasons))
 	copy(errs, wrapper.reasons)
 	return errs
+}
+
+func (wrapper *Wrapper) ExitCode() int {
+	return 1
 }
