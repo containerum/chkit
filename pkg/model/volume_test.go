34296dbe0155797bb834ca6624decdadb6e44609
paul <petrukhin.paul@gmail.com>
2018-03-13 12:33:45 +0300

    tests

--- pkg/model/volume_test.go
+++ pkg/model/volume_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestVolumeTableRendering(test *testing.T) {
 	volume := new(Volume)
 	headers := volume.TableHeaders()
-	row := volume.TableRow()
+	row := volume.TableRows()[0]
 	if len(headers) != len(row) {
 		test.Logf("\nHeaders: %v\nRow: %v", headers, row)
 		test.Fatalf("num of headers and len of row are not equal!")
