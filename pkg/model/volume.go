ff9c4d8ff3851d4b1e25ac38a6521a58d9c28075
paul <petrukhin.paul@gmail.com>
2018-03-16 10:08:20 +0300

    add VolumeListFromKube

--- pkg/model/volume.go
+++ pkg/model/volume.go
@@ -18,6 +18,14 @@ var (
 
 type VolumeList []Volume
 
+func VolumeListFromKube(kubeList []kubeModels.Volume) VolumeList {
+	var list VolumeList = make([]Volume, 0, len(kubeList))
+	for _, volume := range kubeList {
+		list = append(list, VolumeFromKube(volume))
+	}
+	return list
+}
+
 func (_ VolumeList) TableHeaders() []string {
 	return new(Volume).TableHeaders()
 }
