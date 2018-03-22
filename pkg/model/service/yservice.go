9663312f1611eb4b4c63aaff5589db2150cf022b
paul <petrukhin.paul@gmail.com>
2018-03-22 15:10:51 +0300

    add service yaml rendering

---
+++ pkg/model/service/yservice.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	"github.com/containerum/chkit/pkg/model"
+	yaml "gopkg.in/yaml.v2"
+)
+
+var (
+	_ model.YAMLrenderer = new(Service)
+)
+
+func (serv *Service) RenderYAML() (string, error) {
+	data, err := yaml.Marshal(serv)
+	return string(data), err
+}
