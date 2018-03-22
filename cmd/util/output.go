6a557fc76a1e2b8a45577cfe23e478a2ed7106a6
paul <petrukhin.paul@gmail.com>
2018-03-22 12:19:07 +0300

    add output func

---
+++ cmd/util/output.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/containerum/chkit/pkg/model"
+	"gopkg.in/urfave/cli.v2"
+)
+
+func WriteData(ctx *cli.Context, renderer model.Renderer) error {
+	var err error
+	var data string
+	output := "stdout"
+	switch {
+	case ctx.IsSet("json"):
+		output = ctx.String("json")
+		data, err = renderer.RenderJSON()
+	case ctx.IsSet("yaml"):
+		output = ctx.String("yaml")
+		data, err = renderer.RenderYAML()
+	}
+	if err != nil {
+		return err
+	}
+	if output == "stdout" {
+		fmt.Println(data)
+		return nil
+	}
+	return ioutil.WriteFile(output, []byte(data), os.ModePerm)
+}
