e008a2d7e2fa5f5e8cb81b314e819b7d03669c88
paul <petrukhin.paul@gmail.com>
2018-03-22 15:09:08 +0300

    add port yaml rendering

---
+++ pkg/model/service/yport.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	"github.com/containerum/chkit/pkg/model"
+	yaml "gopkg.in/yaml.v2"
+)
+
+var (
+	_ model.YAMLrenderer = new(Port)
+)
+
+func (port *Port) RenderYAML() (string, error) {
+	data, err := yaml.Marshal(port)
+	return string(data), err
+}
