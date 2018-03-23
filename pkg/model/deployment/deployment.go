d5c11c3b4966e8b87abdd2099e80a4a1b42f2c09
paul <petrukhin.paul@gmail.com>
2018-03-23 11:04:10 +0300

    add constructor

--- pkg/model/deployment/deployment.go
+++ pkg/model/deployment/deployment.go
@@ -1,8 +1,32 @@
 package deployment
 
+import (
+	"git.containerum.net/ch/kube-client/pkg/model"
+)
+
 type Deployment struct {
-	Name     string
-	Replicas int
-	Status   *Status
-	Volumes  []Volume
+	Name       string
+	Replicas   uint
+	Status     *Status
+	Containers []Container
+	origin     model.Deployment
+}
+
+func DeploymentFromKube(kubeDeployment model.Deployment) Deployment {
+	var status *Status
+	if kubeDeployment.Status != nil {
+		st := StatusFromKubeStatus(*kubeDeployment.Status)
+		status = &st
+	}
+	containers := make([]Container, 0, len(kubeDeployment.Containers))
+	for _, kubeContainer := range kubeDeployment.Containers {
+		containers = append(containers, Container{kubeContainer})
+	}
+	return Deployment{
+		Name:       kubeDeployment.Name,
+		Replicas:   uint(kubeDeployment.Replicas),
+		Status:     status,
+		Containers: containers,
+		origin:     kubeDeployment,
+	}
 }
