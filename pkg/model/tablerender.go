0a7311284249350fc707d2d0d14b5aa3e9350b44
paul <petrukhin.paul@gmail.com>
2018-03-23 15:01:29 +0300

    enable autowrap

--- pkg/model/tablerender.go
+++ pkg/model/tablerender.go
@@ -17,7 +17,7 @@ type TableRenderer interface {
 func RenderTable(renderer TableItem) string {
 	buf := &bytes.Buffer{}
 	table := tablewriter.NewWriter(buf)
-	table.SetAutoWrapText(false)
+	table.SetAutoWrapText(true)
 	table.SetHeader(renderer.TableHeaders())
 	table.AppendBulk(renderer.TableRows())
 	table.Render()
