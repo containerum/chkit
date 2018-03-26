ee0967f46ce8dfa60fd3107830a7ec7cfc31b600
paul <petrukhin.paul@gmail.com>
2018-03-26 17:06:26 +0300

    change timestamp format

--- pkg/model/common.go
+++ pkg/model/common.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -9,5 +10,24 @@ const (
 )
 
 func TimestampFormat(timestamp time.Time) string {
-	return timestamp.UTC().Format(CreationTimeFormat)
+	age := time.Now().Sub(timestamp)
+	ageString := age.String()
+	switch {
+	case age.Hours() > 24:
+		days := uint64(age.Hours()) / 24
+		hours := uint64(age.Hours()) % 24
+		ageString = fmt.Sprintf("%dd%02dh", days, hours)
+	case age.Hours() <= 24 && age.Hours() > 1:
+		hours := uint64(age.Hours())
+		minutes := uint64(age.Minutes()) % 60
+		ageString = fmt.Sprintf("%dh%02dm", hours, minutes)
+	case age.Hours() < 1 && age.Minutes() > 1:
+		minutes := uint64(age.Minutes())
+		seconds := uint64(age.Seconds()) % 60
+		ageString = fmt.Sprintf("%dm%02ds", minutes, seconds)
+	default:
+		seconds := uint64(age.Seconds())
+		ageString = fmt.Sprintf("%ds", seconds)
+	}
+	return ageString
 }
