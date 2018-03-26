485cfe0571b5c4b942ed0e5cba15d6a4619b9fef
paul <petrukhin.paul@gmail.com>
2018-03-26 17:47:45 +0300

    save tokens after request

--- cmd/get.go
+++ cmd/get.go
@@ -5,6 +5,7 @@ import (
 	"github.com/containerum/chkit/cmd/namespace"
 	"github.com/containerum/chkit/cmd/pod"
 	"github.com/containerum/chkit/cmd/service"
+	"github.com/containerum/chkit/cmd/util"
 	"gopkg.in/urfave/cli.v2"
 )
 
@@ -23,6 +24,10 @@ var commandGet = &cli.Command{
 	         get ns                  --> show namespace list`,
 	Action: func(ctx *cli.Context) error {
 		return ctx.App.Command("help").Run(ctx)
+	},
+	After: func(ctx *cli.Context) error {
+		client := util.GetClient(ctx)
+		return util.SaveTokens(ctx, client.Tokens)
 	},
 	Subcommands: []*cli.Command{
 		clinamespace.GetNamespace,
