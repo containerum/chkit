a0a5cb9b04e920833acfe54fb0227eef3c3f2071
paul <petrukhin.paul@gmail.com>
2018-03-27 10:12:44 +0300

    add recover go

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"os"
-
-	"github.com/sirupsen/logrus"
+	"runtime/debug"
 
 	"github.com/containerum/chkit/cmd"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	defer func() {
+		recoverData := recover()
+		if recoverData != nil {
+			fmt.Printf("[FATAL] %v\n%s", recoverData, string(debug.Stack()))
+		}
+	}()
 	switch err := cmd.Run(os.Args).(type) {
 	case nil:
 	default:
