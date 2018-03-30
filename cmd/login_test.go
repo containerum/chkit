827651f0c7a5c201a436677e5db63e614d59f4fa
paul <petrukhin.paul@gmail.com>
2018-03-30 11:09:38 +0300

    fix test

--- cmd/login_test.go
+++ cmd/login_test.go
@@ -9,11 +9,15 @@ import (
 
 func TestLoginToSandbox(test *testing.T) {
 	client, err := chClient.NewClient(model.Config{
-		APIaddr:     "https://192.168.88.200:8082",
-		Username:    "helpik94@yandex.ru",
-		Password:    "12345678",
+		APIaddr: "https://192.168.88.200:8082",
+		StorableConfig: model.StorableConfig{
+			UserInfo: model.UserInfo{
+				Username: "helpik94@yandex.ru",
+				Password: "12345678",
+			},
+		},
 		Fingerprint: Fingerprint(),
-	}, chClient.UnsafeSkipTLSCheck)
+	}, chClient.WithTestAPI)
 	if err != nil {
 		test.Fatalf("error while client creation: %v", err)
 	}
