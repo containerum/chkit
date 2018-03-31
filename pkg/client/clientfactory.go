5d3649b4c600df9e1b12edccd850be87c5eaf63c
Vladimir Stolyarov <xakep6666@gmail.com>
2018-03-31 12:34:34 +0300

    Use new client API.

--- pkg/client/clientfactory.go
+++ pkg/client/clientfactory.go
@@ -5,12 +5,11 @@ import (
 
 	"time"
 
-	"net"
-
 	kubeClient "git.containerum.net/ch/kube-client/pkg/client"
 	"git.containerum.net/ch/kube-client/pkg/rest/re"
 	"git.containerum.net/ch/kube-client/pkg/rest/remock"
 	"git.containerum.net/ch/kube-client/pkg/websocket/wsmock"
+	"git.containerum.net/ch/kube-client/pkg/websocket/gorilla"
 	"github.com/containerum/chkit/pkg/model"
 	"github.com/gorilla/websocket"
 	"github.com/sirupsen/logrus"
@@ -28,12 +27,11 @@ var (
 // WithCommonAPI -- creates kube-client for production api
 func WithCommonAPI(config model.Config) (*kubeClient.Client, error) {
 	client, err := kubeClient.NewClient(kubeClient.Config{
-		APIurl:  config.APIaddr,
 		RestAPI: re.NewResty(re.WithHost(config.APIaddr)),
 		User: kubeClient.User{
 			Role: "user",
 		},
-		WSDialer: websocket.DefaultDialer,
+		WSDialer: gorilla.NewWebsocket(gorilla.WithHost(config.APIaddr)),
 	})
 	if err != nil {
 		return nil, err
@@ -45,67 +43,50 @@ func WithCommonAPI(config model.Config) (*kubeClient.Client, error) {
 
 // WithTestAPI -- creates kube-client for test api
 func WithTestAPI(config model.Config) (*kubeClient.Client, error) {
-	var newRestAPI *re.Resty
-	if config.Log == nil {
-		newRestAPI = re.NewResty(
-			re.WithHost(config.APIaddr),
-			re.SkipTLSVerify)
-	} else {
-		newRestAPI = re.NewResty(
-			re.WithLogger(config.Log),
-			re.WithHost(config.APIaddr),
-			re.SkipTLSVerify)
+	dialer := &websocket.Dialer{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
-	newRestAPI.SetFingerprint(config.Fingerprint)
-	newRestAPI.SetToken(config.Tokens.AccessToken)
 	client, err := kubeClient.NewClient(kubeClient.Config{
-		APIurl:  config.APIaddr,
-		RestAPI: newRestAPI,
+		RestAPI: re.NewResty(re.WithHost(config.APIaddr), re.WithLogger(config.Log), re.SkipTLSVerify),
 		User: kubeClient.User{
 			Role: "user",
 		},
-		WSDialer: &websocket.Dialer{
-			ReadBufferSize:  1024,
-			WriteBufferSize: 1024,
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		},
+		WSDialer: gorilla.NewWebsocket(gorilla.WithHost(config.APIaddr), gorilla.WithDialer(dialer)),
 	})
 	if err != nil {
 		return nil, err
 	}
+	client.SetFingerprint(config.Fingerprint)
+	client.SetToken(config.Tokens.AccessToken)
 	return client, nil
 }
 
 // WithMock -- creates kube-client with mock API
 func WithMock(config model.Config) (*kubeClient.Client, error) {
-	newRestAPI := remock.NewMock()
-	newRestAPI.SetFingerprint(config.Fingerprint)
-	newRestAPI.SetToken(config.Tokens.AccessToken)
-
 	mockServer := wsmock.NewPeriodicServer(wsmock.PeriodicServerConfig{
 		MsgPeriod: time.Second,
 		MsgText:   "test\n",
 	}, logrus.NewEntry(logrus.StandardLogger()), true)
 
+	dialer := &websocket.Dialer{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
 	client, err := kubeClient.NewClient(kubeClient.Config{
-		APIurl:  config.APIaddr,
-		RestAPI: newRestAPI,
+		RestAPI: remock.NewMock(),
 		User: kubeClient.User{
 			Role: "user",
 		},
-		WSDialer: &websocket.Dialer{
-			WriteBufferSize: 1024,
-			ReadBufferSize:  1024,
-			NetDial: func(network, addr string) (net.Conn, error) {
-				return net.Dial("tcp", mockServer.URL().Host)
-			},
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		},
+		WSDialer: gorilla.NewWebsocket(gorilla.WithHost(mockServer.URL().String()), gorilla.WithDialer(dialer)),
 	})
 	if err != nil {
 		return nil, err
 	}
+	client.SetToken(config.Tokens.AccessToken)
+	client.SetFingerprint(config.Fingerprint)
 	return client, nil
 }
