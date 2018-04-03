dfa37d14710b4cf0dfbe9af1b09d82eed91b35f9
paul <petrukhin.paul@gmail.com>
2018-04-03 16:27:50 +0300

    add name astera name generator

--- pkg/util/namegen/gen.go
+++ pkg/util/namegen/gen.go
@@ -9,8 +9,12 @@ var (
 	rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 )
 
-func GenerateName() string {
+func ColoredPhysics() string {
 	i := rnd.Intn(len(Colors))
 	j := rnd.Intn(len(Physicists))
 	return Colors[i] + "-" + Physicists[j]
+}
+
+func Aster() string {
+	return Asteroids[rnd.Intn(len(Asteroids))]
 }
