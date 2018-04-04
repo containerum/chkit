57d64bab53982c584cb4f136ec975d468f11b101
paul <petrukhin.paul@gmail.com>
2018-04-04 16:29:22 +0300

    add ToKube model

--- pkg/model/service/service.go
+++ pkg/model/service/service.go
@@ -15,7 +15,7 @@ type Service struct {
 	IPs       []string
 	Domain    string
 	Ports     []Port
-	origin    kubeModels.Service
+	origin    *kubeModels.Service
 }
 
 func ServiceFromKube(kubeService kubeModels.Service) Service {
@@ -39,6 +39,30 @@ func ServiceFromKube(kubeService kubeModels.Service) Service {
 		IPs:       kubeService.IPs,
 		Domain:    kubeService.Domain,
 		Ports:     ports,
-		origin:    kubeService,
+		origin:    &kubeService,
 	}
+}
+
+func (serv *Service) ToKube() kubeModels.Service {
+	if serv.origin != nil {
+		return *serv.origin
+	}
+	kubeServ := kubeModels.Service{
+		Name:   serv.Name,
+		Deploy: serv.Deploy,
+		IPs:    serv.IPs,
+		Domain: serv.Domain,
+	}
+	ports := make([]kubeModels.ServicePort, 0, len(serv.Ports))
+	for _, port := range serv.Ports {
+		ports = append(ports, kubeModels.ServicePort(kubeModels.ServicePort{
+			Name:       port.Name,
+			Port:       port.Port,
+			TargetPort: port.TargetPort,
+			Protocol:   kubeModels.Protocol(port.Protocol),
+		}))
+	}
+	kubeServ.Ports = ports
+	serv.origin = &kubeServ
+	return *serv.origin
 }
