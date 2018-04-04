c0ef24edd0638a529c5eb40ab9805f7f5a300e22
paul <petrukhin.paul@gmail.com>
2018-04-04 13:34:10 +0300

    add init

--- cmd/create.go
+++ cmd/create.go
@@ -2,11 +2,16 @@ package cmd
 
 import (
 	"github.com/containerum/chkit/cmd/service"
+	"github.com/sirupsen/logrus"
 	"gopkg.in/urfave/cli.v2"
 )
 
 var CommandCreate = &cli.Command{
 	Name: "create",
+	Before: func(ctx *cli.Context) error {
+		logrus.Debugf("start create action")
+		return setupAll(ctx)
+	},
 	Action: func(ctx *cli.Context) error {
 		return cli.ShowSubcommandHelp(ctx)
 	},
