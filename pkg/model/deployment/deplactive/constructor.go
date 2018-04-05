41eab7c31c4250f80ab30ca9da347bef7b1136c7
paul <petrukhin.paul@gmail.com>
2018-04-05 15:59:23 +0300

    add deployment construstor

--- pkg/model/deployment/deplactive/constructor.go
+++ pkg/model/deployment/deplactive/constructor.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 
 	"github.com/containerum/chkit/pkg/chkitErrors"
+	"github.com/containerum/chkit/pkg/model/container"
 	"github.com/containerum/chkit/pkg/model/deployment"
 	"github.com/containerum/chkit/pkg/util/activeToolkit"
 	"github.com/containerum/chkit/pkg/util/namegen"
+	"github.com/containerum/chkit/pkg/util/validation"
 )
 
 const (
-	ErrUserStopped chkitErrors.Err = "user stopped"
+	ErrUserStopped      chkitErrors.Err = "user stopped"
+	ErrInvalidContainer chkitErrors.Err = "invalid container"
 )
 
 type Config struct {
@@ -22,19 +25,13 @@ func RunInteractveConstructor(config Config) (deployment.DeploymentList, error) 
 	return nil, nil
 }
 
-func constructDeployment(config Config) (deployment.Deployment, error) {
+func ConstructDeployment(config Config) (deployment.Deployment, error) {
 	depl := defaultDeployment()
 	for {
 		_, n, _ := activeToolkit.Options("Whats't next?", false,
 			fmt.Sprintf("Set name     : %s", depl.Name),
 			fmt.Sprintf("Set replicas : %d", depl.Replicas),
-			fmt.Sprintf("Set containers: %v", func() string {
-				if len(depl.Containers) == 0 {
-					return "none (required)"
-				} else {
-					return fmt.Sprintf("%v", depl.Containers)
-				}
-			}()),
+			fmt.Sprintf("Set containers: %v", activeToolkit.OrValue(depl.Containers, "none (required)")),
 			"Confirm",
 			"Exit")
 		switch n {
@@ -43,7 +40,7 @@ func constructDeployment(config Config) (deployment.Deployment, error) {
 		case 1:
 			depl.Replicas = getReplicas(depl.Replicas)
 		case 2:
-
+			depl.Containers = getContainers()
 		case 3:
 			return depl, nil
 		default:
@@ -58,4 +55,24 @@ func defaultDeployment() deployment.Deployment {
 		Replicas:   1,
 		Containers: nil,
 	}
+}
+
+func validateContainer(cont container.Container) error {
+	var errs []error
+	if err := validation.ValidateLabel(cont.Name); err != nil {
+		errs = append(errs, err)
+	}
+	if err := validation.ValidateImageName(cont.Image); err != nil {
+		errs = append(errs, err)
+	}
+	if cont.Limits.CPU == "" {
+		errs = append(errs, fmt.Errorf("undefined CPU limit"))
+	}
+	if cont.Limits.Memory == "" {
+		errs = append(errs, fmt.Errorf("undefined memory limit"))
+	}
+	if len(errs) > 0 {
+		return ErrInvalidContainer.Wrap(errs...)
+	}
+	return nil
 }
