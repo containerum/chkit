ae9ed63008f9abdedbcd9423ea627de0e739c76b
paul <petrukhin.paul@gmail.com>
2018-04-05 15:58:31 +0300

    add image name  validation

--- pkg/util/validation/validation.go
+++ pkg/util/validation/validation.go
@@ -4,15 +4,33 @@ import (
 	"regexp"
 
 	"github.com/containerum/chkit/pkg/chkitErrors"
+	"github.com/docker/distribution/reference"
 )
 
 const (
-	ErrInvalidLabel chkitErrors.Err = "invalid label"
+	ErrInvalidLabel         chkitErrors.Err = "invalid label"
+	ErrInvalidImageName     chkitErrors.Err = "invalid image name"
+	ErrInvalidContainerName chkitErrors.Err = "invalid container name"
 )
 
 var (
-	labelRe = regexp.MustCompile("^[a-z0-9]([-a-z0-9]*[a-z0-9])?$")
+	labelRe         = regexp.MustCompile("^[a-z0-9]([-a-z0-9]*[a-z0-9])?$")
+	containerNameRe = regexp.MustCompile("^[a-z0-9](([a-z0-9-[^-])){1,61}[a-z0-9]$")
 )
+
+func ValidateContainerName(name string) error {
+	if !containerNameRe.MatchString(name) {
+		return ErrInvalidContainerName
+	}
+	return nil
+}
+
+func ValidateImageName(image string) error {
+	if !reference.NameRegexp.MatchString(image) {
+		return ErrInvalidImageName
+	}
+	return nil
+}
 
 func ValidateLabel(label string) error {
 	if !labelRe.MatchString(label) {
