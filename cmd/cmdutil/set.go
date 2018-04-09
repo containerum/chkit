ecb3bfcfa20712c9a65177b4dc24431fa47652f1
paul <petrukhin.paul@gmail.com>
2018-04-09 17:34:31 +0300

    add slice method

--- cmd/cmdutil/set.go
+++ cmd/cmdutil/set.go
@@ -13,3 +13,11 @@ func (s Set) Have(v string) bool {
 	_, ok := s[v]
 	return ok
 }
+
+func (s Set) Slice() []string {
+	slice := make([]string, 0, len(s))
+	for item := range s {
+		slice = append(slice, item)
+	}
+	return slice
+}
