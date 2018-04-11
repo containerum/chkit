6ed3d309a44daa531c7ba4ba844fa115511818ce
paul <petrukhin.paul@gmail.com>
2018-04-11 13:43:03 +0300

    add storable config

--- pkg/context/context.go
+++ pkg/context/context.go
@@ -9,18 +9,41 @@ import (
 	"github.com/containerum/chkit/pkg/model"
 )
 
-var Context = struct {
-	Version     string
-	ConfigPath  string
-	ConfigDir   string
-	APIaddr     string
-	Fingerprint string
-	Namespace   string
-	Quiet       bool
-	Tokens      model.Tokens
-	Client      chClient.Client
-}{
+var Context = context{
 	Version:    semver.MustParse("3.0.1-alpha").String(),
 	ConfigDir:  configdir.ConfigDir(),
 	ConfigPath: path.Join(configdir.ConfigDir(), "config.toml"),
+}
+
+type context struct {
+	Version     string
+	ConfigPath  string
+	ConfigDir   string
+	Fingerprint string
+	Namespace   string
+	Quiet       bool
+	Changed     bool
+	Client      chClient.Client
+}
+
+type Storable struct {
+	Namespace string
+	Username  string
+	Password  string
+}
+
+func (ctx *context) GetStorable() Storable {
+	return Storable{
+		Namespace: ctx.Namespace,
+		Username:  ctx.Client.Username,
+		Password:  ctx.Client.Password,
+	}
+}
+
+func (ctx *context) SetStorable(config Storable) {
+	ctx.Namespace = config.Namespace
+	ctx.Client.UserInfo = model.UserInfo{
+		Username: config.Username,
+		Password: config.Password,
+	}
 }
