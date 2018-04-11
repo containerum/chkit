5494d94da698fd1fb9bd58e08fb8a378b079a7c5
paul <petrukhin.paul@gmail.com>
2018-04-11 12:19:12 +0300

    use cobra

--- pkg/cli/deployment/get.go
+++ pkg/cli/deployment/get.go
@@ -1,61 +1,80 @@
 package clideployment
 
 import (
+	"fmt"
 	"strings"
 
-	"github.com/containerum/chkit/cmd/util"
+	"github.com/containerum/chkit/pkg/configuration"
+
 	"github.com/containerum/chkit/pkg/chkitErrors"
+	. "github.com/containerum/chkit/pkg/context"
 	"github.com/containerum/chkit/pkg/model"
 	"github.com/containerum/chkit/pkg/model/deployment"
+	"github.com/containerum/chkit/pkg/util/angel"
+	"github.com/containerum/chkit/pkg/util/strset"
 	"github.com/sirupsen/logrus"
-	"gopkg.in/urfave/cli.v2"
+	"github.com/spf13/cobra"
 )
 
-var (
+const (
 	ErrNoNamespaceSpecified chkitErrors.Err = "no namespace specified"
 )
 
 var aliases = []string{"depl", "deployments", "deploy"}
-
-var GetDeployment = &cli.Command{
-	Name:        "deployment",
-	Aliases:     aliases,
-	Usage:       "shows deployment data",
-	Description: "shows deployment data. Aliases: " + strings.Join(aliases, ", "),
-	UsageText:   "namespace deployment_names... [-n namespace_label]",
-	Action: func(ctx *cli.Context) error {
-		if ctx.Bool("help") {
-			return cli.ShowSubcommandHelp(ctx)
-		}
-		client := util.GetClient(ctx)
-		defer util.StoreClient(ctx, client)
-
-		var show model.Renderer
-		switch ctx.NArg() {
-		case 0:
-			namespace := util.GetNamespace(ctx)
-			logrus.Debugf("getting deployment from %q", namespace)
-			list, err := client.GetDeploymentList(namespace)
-			if err != nil {
-				return err
-			}
-			show = list
-		default:
-			namespace := util.GetNamespace(ctx)
-			deplNames := util.NewSet(ctx.Args().Slice())
-			var showList deployment.DeploymentList = make([]deployment.Deployment, 0) // prevents panic
-			list, err := client.GetDeploymentList(namespace)
-			if err != nil {
-				return err
-			}
-			for _, depl := range list {
-				if deplNames.Have(depl.Name) {
-					showList = append(showList, depl)
-				}
-			}
-			show = showList
-		}
-		return util.ExportDataCommand(ctx, show)
+var getDeplDataConfig = struct {
+	configuration.ExportConfig
+}{
+	configuration.ExportConfig{
+		Format: configuration.PRETTY,
 	},
-	Flags: util.GetFlags,
+}
+
+var Get = &cobra.Command{
+	Use:     "deployment",
+	Short:   "shows deployment data",
+	Long:    "shows deployment data. Aliases: " + strings.Join(aliases, ", "),
+	Example: "namespace deployment_names... [-n namespace_label]",
+	Aliases: aliases,
+	Run: func(command *cobra.Command, args []string) {
+		deplData, err := func() (model.Renderer, error) {
+			switch len(args) {
+			case 0:
+				logrus.Debugf("getting deployment from %q", Context.Namespace)
+				list, err := Context.Client.GetDeploymentList(Context.Namespace)
+				if err != nil {
+					return nil, err
+				}
+				return list, nil
+			default:
+				deplNames := strset.NewSet(args)
+				var showList deployment.DeploymentList = make([]deployment.Deployment, 0) // prevents panic
+				list, err := Context.Client.GetDeploymentList(Context.Namespace)
+				if err != nil {
+					return nil, err
+				}
+				for _, depl := range list {
+					if deplNames.Have(depl.Name) {
+						showList = append(showList, depl)
+					}
+				}
+				return showList, nil
+			}
+		}()
+		if err != nil {
+			logrus.WithError(err).Errorf("unable to get deployment data")
+			fmt.Printf("%v :(\n", err)
+			return
+		}
+		if err := configuration.ExportData(deplData, configuration.ExportConfig{}); err != nil {
+			logrus.WithError(err).Errorf("unable to export data")
+			angel.Angel(err)
+		}
+	},
+}
+
+func init() {
+	Get.PersistentFlags().
+		StringVarP((*string)(&getDeplDataConfig.Format), "output", "o", "", "output format (yaml/json)")
+	Get.PersistentFlags().
+		StringVarP(getDeplDataConfig.Filename, "file", "f", "", "output file")
 }
