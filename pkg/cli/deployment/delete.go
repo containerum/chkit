a85c0f25d33df614e96f82fe19e1deeb40b95227
paul <petrukhin.paul@gmail.com>
2018-04-18 12:02:35 +0300

    add menu with deployment selection

--- pkg/cli/deployment/delete.go
+++ pkg/cli/deployment/delete.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/containerum/chkit/pkg/context"
+	"github.com/containerum/chkit/pkg/model/deployment"
 	"github.com/containerum/chkit/pkg/util/activekit"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -22,23 +23,59 @@ func Delete(ctx *context.Context) *cobra.Command {
 		Short:   "call to delete deployment in specific namespace",
 		Long:    "call to delete deployment in specific namespace. Aliases: " + strings.Join(aliases, ", "),
 		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) != 1 {
+			switch len(args) {
+			case 0:
+				list, err := ctx.Client.GetDeploymentList(ctx.Namespace)
+				if err != nil {
+					logrus.WithError(err).Errorf("unable to get deployment list")
+					activekit.Attention(err.Error())
+				}
+				var menu []*activekit.MenuItem
+				for _, depl := range list {
+					menu = append(menu, &activekit.MenuItem{
+						Label: depl.Name,
+						Action: func(depl deployment.Deployment) func() error {
+							return func() error {
+								if activekit.YesNo(fmt.Sprintf("Are you sure you want to delete deployment %q? [Y/N]: ", depl.Name)) {
+									if err := ctx.Client.DeleteDeployment(ctx.Namespace, depl.Name); err != nil {
+										logrus.WithError(err).Debugf("unable to delete deployment %q in namespace %q", depl.Name, ctx.Namespace)
+										activekit.Attention(err.Error())
+										os.Exit(1)
+									}
+									fmt.Printf("OK\n")
+									logrus.Debugf("deployment %q in namespace %q deleted", depl.Name, ctx.Namespace)
+								}
+								return nil
+							}
+						}(depl),
+					})
+				}
+				(&activekit.Menu{
+					Items: append(menu, []*activekit.MenuItem{
+						{
+							Label: "Exit",
+						},
+					}...),
+				}).Run()
+			case 1:
+				deplName := args[0]
+				logrus.
+					WithField("command", "delete deployment").
+					Debugf("start deleting deployment %q", deplName)
+				if deleteDeplConfig.Force || activekit.YesNo(fmt.Sprintf("Are you sure you want to delete deployment %q? [Y/N]: ", deplName)) {
+					if err := ctx.Client.DeleteDeployment(ctx.Namespace, deplName); err != nil {
+						logrus.WithError(err).Debugf("unable to delete deployment %q in namespace %q", deplName, ctx.Namespace)
+						activekit.Attention(err.Error())
+						os.Exit(1)
+					}
+					fmt.Printf("OK\n")
+					logrus.Debugf("deployment %q in namespace %q deleted", deplName, ctx.Namespace)
+				}
+			default:
 				cmd.Help()
 				return
 			}
-			deplName := args[0]
-			logrus.
-				WithField("command", "delete deployment").
-				Debugf("start deleting deployment %q", deplName)
-			if deleteDeplConfig.Force || activekit.YesNo(fmt.Sprintf("Are you sure you want to delete deployment %q? [Y/N]: ", deplName)) {
-				if err := ctx.Client.DeletePod(ctx.Namespace, deplName); err != nil {
-					logrus.WithError(err).Debugf("unable to delete deployment %q in namespace %q", deplName, ctx.Namespace)
-					activekit.Attention(err.Error())
-					os.Exit(1)
-				}
-				fmt.Printf("OK\n")
-				logrus.Debugf("deployment %q in namespace %q deleted", deplName, ctx.Namespace)
-			}
+
 		},
 	}
 	command.PersistentFlags().
