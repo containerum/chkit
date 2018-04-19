2fcc1398e888ffe3e55e3342a7db210ab9ddff00
paul <petrukhin.paul@gmail.com>
2018-04-19 12:50:39 +0300

    Merge branch 'add-set-replicas-command' into chkit-v3 Closes #55

--- pkg/cli/clisetup/client.go
+++ pkg/cli/clisetup/client.go
@@ -3,25 +3,25 @@ package clisetup
 import (
 	"github.com/containerum/chkit/pkg/cli/mode"
 	"github.com/containerum/chkit/pkg/client"
-	. "github.com/containerum/chkit/pkg/context"
+	"github.com/containerum/chkit/pkg/context"
 	"github.com/sirupsen/logrus"
 )
 
 func SetupClient() error {
 	var err error
 	if mode.DEBUG && !mode.MOCK {
-		logrus.WithField("operation", "SetupClient").Debugf("Using test API: %q", GlobalContext.Client.APIaddr)
+		logrus.WithField("operation", "SetupClient").Debugf("Using test API: %q", context.GlobalContext.Client.APIaddr)
 		if Config.DebugRequests {
 			logrus.Debugf("verbose requests logs")
-			GlobalContext.Client.Log = logrus.StandardLogger().WriterLevel(logrus.DebugLevel)
+			context.GlobalContext.Client.Log = logrus.StandardLogger().WriterLevel(logrus.DebugLevel)
 		}
-		err = GlobalContext.Client.Init(chClient.WithTestAPI)
+		err = context.GlobalContext.Client.Init(chClient.WithTestAPI)
 	} else if mode.DEBUG && mode.MOCK {
 		logrus.Debugf("Using mock API")
-		err = GlobalContext.Client.Init(chClient.WithMock)
+		err = context.GlobalContext.Client.Init(chClient.WithMock)
 	} else {
-		logrus.Debugf("Using production API: %v", GlobalContext.Client.APIaddr)
-		err = GlobalContext.Client.Init(chClient.WithCommonAPI)
+		logrus.Debugf("Using production API: %v", context.GlobalContext.Client.APIaddr)
+		err = context.GlobalContext.Client.Init(chClient.WithCommonAPI)
 	}
 	if err != nil {
 		return err
