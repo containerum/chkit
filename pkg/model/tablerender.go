1c91bf4304b6c8a30cf9d975509867b44f95b9e2
paul <petrukhin.paul@gmail.com>
2018-04-20 15:53:23 +0300

    fix table rendering

--- pkg/model/tablerender.go
+++ pkg/model/tablerender.go
@@ -16,8 +16,14 @@ type TableRenderer interface {
 
 func RenderTable(renderer TableItem) string {
 	buf := &bytes.Buffer{}
+
 	table := tablewriter.NewWriter(buf)
 	table.SetAutoWrapText(true)
+	table.SetRowSeparator("-")
+	table.SetRowLine(true)
+	table.SetReflowDuringAutoWrap(true)
+	table.SetCenterSeparator("-")
+
 	table.SetHeader(renderer.TableHeaders())
 	table.AppendBulk(renderer.TableRows())
 	table.Render()
