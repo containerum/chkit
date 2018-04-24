2a7daffb121c87c4478b72a0619623857da4ff45
paul <petrukhin.paul@gmail.com>
2018-04-24 17:09:01 +0300

    add ingress simple table view

---
+++ pkg/model/ingress/ingress_table.go
@@ -0,0 +1,32 @@
+package ingress
+
+import (
+	"strings"
+
+	"github.com/containerum/chkit/pkg/model"
+)
+
+var (
+	_ model.TableRenderer = Ingress{}
+	_ model.TableItem     = Ingress{}
+)
+
+func (ingress Ingress) RenderTable() string {
+	return model.RenderTable(ingress)
+}
+
+func (ingress Ingress) TableHeaders() []string {
+	return []string{
+		"Name",
+		"Host",
+		"Service",
+	}
+}
+
+func (ingress Ingress) TableRows() [][]string {
+	return [][]string{{
+		ingress.Name,
+		strings.Join(ingress.Rules.Hosts(), "\n"),
+		strings.Join(ingress.Rules.ServicesTableView(), "\n"),
+	}}
+}
