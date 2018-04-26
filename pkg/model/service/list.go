bc89645ca2f9a41fe328318ed254886ea8c7eab9
paul <petrukhin.paul@gmail.com>
2018-04-26 17:13:16 +0300

    add names selectors

--- pkg/model/service/list.go
+++ pkg/model/service/list.go
@@ -13,3 +13,20 @@ func ServiceListFromKube(kubeList []model.Service) ServiceList {
 	}
 	return list
 }
+
+func (list ServiceList) Names() []string {
+	names := make([]string, 0, len(list))
+	for _, serv := range list {
+		names = append(names, serv.Name)
+	}
+	return names
+}
+
+func (list ServiceList) GetByName(name string) (Service, bool) {
+	for _, serv := range list {
+		if serv.Name == name {
+			return serv, true
+		}
+	}
+	return Service{}, false
+}
