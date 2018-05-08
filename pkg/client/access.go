cf90ef87880df35ccb21cc8c18b57a18c297c624
paul <petrukhin.paul@gmail.com>
2018-05-08 16:35:54 +0300

    add method "SetAccess"

--- pkg/client/access.go
+++ pkg/client/access.go
@@ -1,6 +1,12 @@
 package chClient
 
-import "github.com/containerum/chkit/pkg/model/access"
+import (
+	"git.containerum.net/ch/auth/pkg/errors"
+	"git.containerum.net/ch/kube-api/pkg/kubeErrors"
+	"github.com/containerum/cherry"
+	"github.com/containerum/chkit/pkg/model/access"
+	"github.com/sirupsen/logrus"
+)
 
 func (client *Client) GetAccess(nsName string) (access.Access, error) {
 	ns, err := client.GetNamespace(nsName)
@@ -10,4 +16,31 @@ func (client *Client) GetAccess(nsName string) (access.Access, error) {
 func (client *Client) GetAccessList() (access.AccessList, error) {
 	list, err := client.GetNamespaceList()
 	return access.AccessListFromNamespaces(list), err
+}
+
+func (client *Client) SetAccess(ns, username string, acc access.AccessLevel) error {
+	err := retry(4, func() (bool, error) {
+		err := client.kubeAPIClient.SetNamespaceAccess(ns, username, acc.String())
+		switch {
+		case err == nil:
+			return false, nil
+		case cherry.In(err,
+			kubeErrors.ErrResourceNotExist(),
+			kubeErrors.ErrAccessError(),
+			kubeErrors.ErrUnableGetResource()):
+			return false, err
+		case cherry.In(err,
+			autherr.ErrInvalidToken(),
+			autherr.ErrTokenNotFound(),
+			autherr.ErrTokenNotOwnedBySender()):
+			return true, client.Auth()
+		default:
+			return true, ErrFatalError.Wrap(err)
+		}
+	})
+	if err != nil {
+		logrus.WithError(err).WithField("namespace", ns).
+			Errorf("unable to set access to namespace")
+	}
+	return err
 }
