d876963ea91873a673d22f8e47097dd3e0bff2fe
paul <petrukhin.paul@gmail.com>
2018-05-08 14:30:18 +0300

    add command "Set containerum-api"

--- pkg/cli/containerumapi/containerumapi.go
+++ pkg/cli/containerumapi/containerumapi.go
@@ -13,16 +13,17 @@ func Set(ctx *context.Context) *cobra.Command {
 	command := &cobra.Command{
 		Use: "containerum-api",
 		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) != 1 {
+			if len(args) != 1 && !cmd.Flags().Changed("allow-self-signed-certs") {
 				cmd.Help()
 				os.Exit(1)
+			} else if len(args) == 1 {
+				api, err := url.Parse(args[0])
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				ctx.Client.APIaddr = api.String()
 			}
-			api, err := url.Parse(args[0])
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			ctx.Client.APIaddr = api.String()
 			ctx.AllowSelfSignedTLS, _ = cmd.Flags().GetBool("allow-self-signed-certs")
 			ctx.Changed = true
 		},
