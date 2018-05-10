5d7a43684cb11f0f942bac094b7013dd25a9d587
paul <petrukhin.paul@gmail.com>
2018-05-10 11:00:17 +0300

    add method "delete access"

--- pkg/client/access.go
+++ pkg/client/access.go
@@ -44,3 +44,30 @@ func (client *Client) SetAccess(ns, username string, acc access.AccessLevel) err
 	}
 	return err
 }
+
+func (client *Client) DeleteAccess(ns, username string) error {
+	err := retry(4, func() (bool, error) {
+		err := client.kubeAPIClient.DeleteNamespaceAccess(ns, username)
+		switch {
+		case err == nil:
+			return false, nil
+		case cherry.In(err,
+			kubeErrors.ErrResourceNotExist(),
+			kubeErrors.ErrAccessError(),
+			kubeErrors.ErrUnableGetResource()):
+			return false, err
+		case cherry.In(err,
+			autherr.ErrInvalidToken(),
+			autherr.ErrTokenNotFound(),
+			autherr.ErrTokenNotOwnedBySender()):
+			return true, client.Auth()
+		default:
+			return true, ErrFatalError.Wrap(err)
+		}
+	})
+	if err != nil {
+		logrus.WithError(err).WithField("namespace", ns).
+			Errorf("unable to delete access to namespace")
+	}
+	return err
+}
