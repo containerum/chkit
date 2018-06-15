6a9b19c899be9478a8774c8c35f2c5ce05bb37f3
paul <petrukhin.paul@gmail.com>
2018-06-15 13:00:17 +0300

    fix timestamp conversion

--- pkg/cli/deployment/replace.go
+++ pkg/cli/deployment/replace.go
@@ -25,7 +25,7 @@ func Replace(ctx *context.Context) *cobra.Command {
 	var flagDepl deployment.Deployment
 	var envs []string
 	command := &cobra.Command{
-		Use:     "deployment",
+		Use:     "deployment-container",
 		Aliases: aliases,
 		Short:   "replace deployment",
 		Long: `Replaces deployment.
