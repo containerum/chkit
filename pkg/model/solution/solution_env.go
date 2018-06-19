20c3fbd212fc5f7e062f764e584d6359922dd952
paul <petrukhin.paul@gmail.com>
2018-06-19 17:33:17 +0300

    add Env and Envs

--- pkg/model/solution/solution_env.go
+++ pkg/model/solution/solution_env.go
@@ -3,6 +3,8 @@ package solution
 import (
 	"fmt"
 
+	"sort"
+
 	kubeModels "github.com/containerum/kube-client/pkg/model"
 )
 
@@ -18,4 +20,50 @@ func (solutionEnv SolutionEnv) String() string {
 		ret += fmt.Sprintf(`%s = %s;`, k, v)
 	}
 	return ret
+}
+
+type Env struct {
+	Name  string
+	Value string
+}
+
+func (env Env) String() string {
+	return env.Name + ":" + env.Value
+}
+
+type Envs []Env
+
+func EnvsFromMap(m map[string]string) Envs {
+	var envs = make(Envs, 0, len(m))
+	for name, value := range m {
+		envs = append(envs, Env{
+			Name:  name,
+			Value: value,
+		})
+	}
+	return envs
+}
+
+func (envs Envs) New() Envs {
+	return make(Envs, 0, len(envs))
+}
+
+func (envs Envs) Copy() Envs {
+	return append(envs.New(), envs...)
+}
+
+func (envs Envs) Sorted() Envs {
+	var sorted = envs.Copy()
+	sort.Slice(sorted, func(i, j int) bool {
+		return sorted[i].Name < sorted[j].Name
+	})
+	return sorted
+}
+
+func (envs Envs) Map() map[string]string {
+	var m = make(map[string]string, len(envs))
+	for _, env := range envs {
+		m[env.Name] = env.Value
+	}
+	return m
 }
