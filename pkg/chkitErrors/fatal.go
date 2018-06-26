4554c54a4e001432f94e2a2f35d0f61a4619e71d
paul <petrukhin.paul@gmail.com>
2018-06-26 10:33:16 +0300

    add fatal error wrapper

---
+++ pkg/chkitErrors/fatal.go
@@ -0,0 +1,38 @@
+package chkitErrors
+
+import (
+	"fmt"
+)
+
+type Fatality interface {
+	Unwrap() error
+	Fatal() string
+	Map(func(err error) error) Fatality
+}
+
+type fatality struct {
+	error
+}
+
+func (fatal fatality) Unwrap() error {
+	return fatal.error
+}
+
+func (fatal fatality) Map(op func(err error) error) Fatality {
+	if fatal.error != nil {
+		return fatality{op(fatal)}
+	}
+	return fatality{}
+}
+
+func (fatal fatality) Fatal() string {
+	return fatal.Error()
+}
+
+func Fatal(err error) Fatality {
+	return fatality{err}
+}
+
+func FatalString(err string, args ...interface{}) Fatality {
+	return fatality{fmt.Errorf(err, args...)}
+}
