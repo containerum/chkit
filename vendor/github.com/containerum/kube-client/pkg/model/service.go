c2f46d883f4c56343f059c2fd4f46e2c27434bb8
Ilia Donchenko <iliad@me.com>
2018-07-04 15:41:45 +0300

    Add yaml tags to models

--- vendor/github.com/containerum/kube-client/pkg/model/service.go
+++ vendor/github.com/containerum/kube-client/pkg/model/service.go
@@ -29,19 +29,19 @@ type ServicesList struct {
 // swagger:model
 type Service struct {
 	// required: true
-	Name string `json:"name"`
+	Name string `json:"name" yaml:"name"`
 	//creation date in RFC3339 format
-	CreatedAt string `json:"created_at,omitempty"`
+	CreatedAt string `json:"created_at,omitempty" yaml:"created_at,omitempty"`
 	//delete date in RFC3339 format
-	DeletedAt string   `json:"deleted_at,omitempty"`
-	Deploy    string   `json:"deploy,omitempty"`
-	IPs       []string `json:"ips,omitempty"`
-	Domain    string   `json:"domain,omitempty"`
+	DeletedAt string   `json:"deleted_at,omitempty" yaml:"deleted_at,omitempty"`
+	Deploy    string   `json:"deploy,omitempty" yaml:"deploy,omitempty"`
+	IPs       []string `json:"ips,omitempty" yaml:"ips,omitempty"`
+	Domain    string   `json:"domain,omitempty" yaml:"domain,omitempty"`
 	//Solution ID (only if service is part of solution)
-	SolutionID string `json:"solution_id,omitempty"`
+	SolutionID string `json:"solution_id,omitempty" yaml:"solution_id,omitempty"`
 	// required: true
-	Ports []ServicePort `json:"ports"`
-	Owner string        `json:"owner,omitempty"`
+	Ports []ServicePort `json:"ports" yaml:"ports"`
+	Owner string        `json:"owner,omitempty" yaml:"owner,omitempty"`
 }
 
 // represent service port
@@ -49,12 +49,12 @@ type Service struct {
 // swagger:model
 type ServicePort struct {
 	// required: true
-	Name string `json:"name"`
-	Port *int   `json:"port,omitempty"`
+	Name string `json:"name" yaml:"name"`
+	Port *int   `json:"port,omitempty" yaml:"port,omitempty"`
 	// required: true
-	TargetPort int `json:"target_port"`
+	TargetPort int `json:"target_port" yaml:"target_port"`
 	// required: true
-	Protocol Protocol `json:"protocol"`
+	Protocol Protocol `json:"protocol" yaml:"protocol"`
 }
 
 // Mask removes information not interesting for users
