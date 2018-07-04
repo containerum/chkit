30c003f30e293215b0106e215738e862334fe135
paul <petrukhin.paul@gmail.com>
2018-07-04 12:40:26 +0300

    add json and yaml unmarshaling

--- pkg/model/deployment/deployment_json.go
+++ pkg/model/deployment/deployment_json.go
@@ -4,11 +4,13 @@ import (
 	"encoding/json"
 
 	"github.com/containerum/chkit/pkg/model"
+	model2 "github.com/containerum/kube-client/pkg/model"
 )
 
 var (
 	_ model.JSONrenderer = Deployment{}
 	_ json.Marshaler     = Deployment{}
+	_ json.Unmarshaler   = new(Deployment)
 )
 
 func (depl Deployment) RenderJSON() (string, error) {
@@ -20,4 +22,13 @@ func (depl Deployment) RenderJSON() (string, error) {
 func (depl Deployment) MarshalJSON() ([]byte, error) {
 	data, err := json.MarshalIndent(depl.ToKube(), "", model.Indent)
 	return data, err
+}
+
+func (depl *Deployment) UnmarshalJSON(data []byte) error {
+	var kubeDepl model2.Deployment
+	if err := json.Unmarshal(data, &kubeDepl); err != nil {
+		return err
+	}
+	*depl = DeploymentFromKube(kubeDepl)
+	return nil
 }
