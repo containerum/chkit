5ee1ee56be687a94c90697d6004fd0ed57b4d7ef
Ilia Donchenko <iliad@me.com>
2018-07-05 16:47:28 +0300

    Make service table output prettier

--- pkg/model/service/service_table.go
+++ pkg/model/service/service_table.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 	"time"
 
+	"fmt"
+
 	"github.com/containerum/chkit/pkg/model"
 	"github.com/ninedraft/boxofstuff/str"
 )
@@ -19,7 +21,7 @@ func (serv Service) RenderTable() string {
 }
 
 func (_ *Service) TableHeaders() []string {
-	return []string{"Name", "Deploy", "URL", "Age"}
+	return []string{"Name", "Deploy", "Kind", "Ports", "Age"}
 }
 
 func (serv *Service) TableRows() [][]string {
@@ -27,31 +29,43 @@ func (serv *Service) TableRows() [][]string {
 	if serv.CreatedAt != (time.Time{}) {
 		age = model.Age(serv.CreatedAt)
 	}
+	kind := "internal"
+
+	var ports = make(str.Vector, 0, len(serv.Ports))
+	for _, p := range serv.Ports {
+		ports = append(ports, fmt.Sprintf("%5d >", *p.Port))
+	}
 	var links = make(str.Vector, 0, len(serv.Ports))
 	if serv.Domain != "" {
+		kind = "external"
 		for _, p := range serv.Ports {
 			switch strings.ToLower(p.Protocol) {
 			case "tcp":
-				links = append(links, (&url.URL{
+				links = append(links, fmt.Sprintf("%s -> %d (%s)", (&url.URL{
 					Scheme: "http",
 					Host:   serv.Domain + ":" + strconv.Itoa(*p.Port),
-				}).String())
+				}).String(), p.TargetPort, p.Protocol))
 			case "udp":
-				links = append(links, (&url.URL{
+				links = append(links, fmt.Sprintf("%s -> %d (%s)", (&url.URL{
 					Scheme: "udp",
 					Host:   serv.Domain + ":" + strconv.Itoa(*p.Port),
-				}).String())
+				}).String(), p.TargetPort, p.Protocol))
 			default:
-				links = append(links, (&url.URL{
+				links = append(links, fmt.Sprintf("%s -> %d (%s)", (&url.URL{
 					Host: serv.Domain + ":" + strconv.Itoa(*p.Port),
-				}).String())
+				}).String(), p.TargetPort, p.Protocol))
 			}
+		}
+	} else {
+		for _, p := range serv.Ports {
+			links = append(links, fmt.Sprintf("%5d -> %d (%s)", *p.Port, p.TargetPort, p.Protocol))
 		}
 	}
 
 	return [][]string{{
 		serv.Name,
 		serv.Deploy,
+		kind,
 		strings.Join(links, "\n"),
 		age,
 	}}
