23d03b02a33d949d4fd61cf69e00e0a157f643b5
Vladimir Stolyarov <xakep6666@gmail.com>
2018-07-06 11:50:27 +0300

    Add external service base test.

--- functional_tests/service.py
+++ functional_tests/service.py
@@ -2,9 +2,10 @@ import unittest
 from timeout_decorator import timeout_decorator
 import functional_tests.chkit as chkit
 import time
+import requests
 
 
-class TestService(unittest.TestCase):
+class TestInternalService(unittest.TestCase):
     __default_services_deployment = chkit.Deployment(
         name="default-services-test-depl",
         replicas=1,
@@ -16,11 +17,11 @@ class TestService(unittest.TestCase):
     @timeout_decorator.timeout(seconds=30)
     @chkit.test_account
     @chkit.with_deployment(deployment=__default_services_deployment)
-    def test_base_internal(self, depl: chkit.Deployment):
+    def test_base(self, depl: chkit.Deployment):
         svc = chkit.Service(
-            name="test-service",
+            name="test-internal-service",
             deploy=depl.name,
-            ports=[chkit.ServicePort(name="test-port", target_port=80, port=8888)]
+            ports=[chkit.ServicePort(name="test-internal-port", target_port=80, port=8888)]
         )
         try:
             chkit.create_service(svc)
@@ -34,20 +35,20 @@ class TestService(unittest.TestCase):
             self.assertNotIn(svc.name, [service.name for service in chkit.get_services()])
 
     __default_internal_service = chkit.Service(
-        name="test-service",
+        name="test-internal-service",
         deploy=__default_services_deployment.name,
-        ports=[chkit.ServicePort(name="test-port", target_port=80, port=8888)]
+        ports=[chkit.ServicePort(name="test-internal-port", target_port=80, port=8888)]
     )
 
     @timeout_decorator.timeout(seconds=30)
     @chkit.test_account
     @chkit.with_deployment(deployment=__default_services_deployment)
     @chkit.with_service(service=__default_internal_service)
-    def test_update_internal(self, depl: chkit.Deployment, svc: chkit.Service):
+    def test_update(self, depl: chkit.Deployment, svc: chkit.Service):
         new_svc = chkit.Service(
             name=svc.name,
             deploy=depl.name,
-            ports=[chkit.ServicePort(name="test-port-1", target_port=80, port=8080)],
+            ports=[chkit.ServicePort(name="test-port-1", target_port=80)],
         )
         chkit.replace_service(new_svc)
         got_svc = chkit.get_service(svc.name)
@@ -56,3 +57,47 @@ class TestService(unittest.TestCase):
         self.assertEqual(got_svc.ports[0].name, new_svc.ports[0].name)
         self.assertEqual(got_svc.ports[0].target_port, new_svc.ports[0].target_port[0])
         self.assertEqual(got_svc.ports[0].port, new_svc.ports[0].target_port)
+
+
+class TestExternalService(unittest.TestCase):
+    __default_services_deployment = chkit.Deployment(
+        name="default-services-test-depl",
+        replicas=1,
+        containers=[
+            chkit.Container(image="nginx", name="first", limits=chkit.Resources(cpu=10, memory=10)),
+        ],
+    )
+
+    @timeout_decorator.timeout(seconds=650*2)
+    @chkit.test_account
+    @chkit.with_deployment(deployment=__default_services_deployment)
+    @chkit.ensure_pods_running(deployment=__default_services_deployment.name)
+    def test_base(self, depl: chkit.Deployment):
+        svc = chkit.Service(
+            name="test-external-service",
+            deploy=depl.name,
+            ports=[chkit.ServicePort(name="test-external-port", target_port=80)]
+        )
+        try:
+            chkit.create_service(svc)
+            self.assertIn(svc.name, [service.name for service in chkit.get_services()])
+            got_svc = chkit.get_service(svc.name)
+            self.assertEqual(svc.name, got_svc.name)
+            self.assertTrue(got_svc.is_external())
+            attempts, max_attempts = 1, 40
+            while attempts <= max_attempts:
+                try:
+                    response = requests.get(f"http://{got_svc.ips[0]}:{got_svc.ports[0].port}",
+                                            headers={"Host": got_svc.domain}, timeout=1)
+                    response.raise_for_status()
+                    if response.status_code == 200:
+                        break
+                except requests.exceptions.ConnectionError:
+                    pass
+                time.sleep(15)
+                attempts += 1
+            self.assertLessEqual(attempts, max_attempts)
+        finally:
+            chkit.delete_service(svc.name)
+            time.sleep(1)
+            self.assertNotIn(svc.name, [service.name for service in chkit.get_services()])
