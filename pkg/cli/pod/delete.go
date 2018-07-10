a33c452e070043bd315a951b86c7a062eb25396b
Ilia Donchenko <iliad@me.com>
2018-07-10 09:32:10 +0300

    Add interactive delete pod

--- pkg/cli/pod/delete.go
+++ pkg/cli/pod/delete.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 
 	"github.com/containerum/chkit/pkg/context"
+	"github.com/containerum/chkit/pkg/export"
+	"github.com/containerum/chkit/pkg/model/pod"
 	"github.com/containerum/chkit/pkg/util/activekit"
+	"github.com/containerum/chkit/pkg/util/angel"
+	"github.com/containerum/chkit/pkg/util/ferr"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -13,6 +17,7 @@ func Delete(ctx *context.Context) *cobra.Command {
 	var deletePodConfig = struct {
 		Force bool
 	}{}
+	exportConfig := export.ExportConfig{}
 	command := &cobra.Command{
 		Use:     "pod",
 		Aliases: aliases,
@@ -20,22 +25,46 @@ func Delete(ctx *context.Context) *cobra.Command {
 		Long:    "Delete pods.",
 		Example: "chkit delete pod pod_name [-n namespace]",
 		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) != 1 {
-				cmd.Help()
-				return
-			}
-			podName := args[0]
-			logrus.
-				WithField("command", "delete pod").
-				Debugf("start deleting pod %q", podName)
-			if deletePodConfig.Force || activekit.YesNo(fmt.Sprintf("Are you sure you want to delete pod %q? [Y/N]: ", podName)) {
-				if err := ctx.Client.DeletePod(ctx.GetNamespace().ID, podName); err != nil {
-					logrus.WithError(err).Debugf("unable to delete pod %q in namespace %q", podName, ctx.GetNamespace())
-					activekit.Attention(err.Error())
+			var selectedPod string
+			if len(args) == 0 && !deletePodConfig.Force {
+				list, err := ctx.Client.GetPodList(ctx.GetNamespace().ID)
+				if err != nil {
+					ferr.Println(err)
 					ctx.Exit(1)
 				}
-				fmt.Printf("OK\n")
-				logrus.Debugf("pod %q in namespace %q deleted", podName, ctx.GetNamespace())
+				if err := export.ExportData(list, exportConfig); err != nil {
+					logrus.WithError(err).Errorf("unable to export data")
+					angel.Angel(ctx, err)
+				}
+				var menu activekit.MenuItems
+				for _, pd := range list {
+					menu = menu.Append(&activekit.MenuItem{
+						Label: pd.Name,
+						Action: func(pd pod.Pod) func() error {
+							return func() error {
+								selectedPod = pd.Name
+								return nil
+							}
+						}(pd.Copy()),
+					})
+				}
+				(&activekit.Menu{
+					Title: "Select pod",
+					Items: menu,
+				}).Run()
+			} else {
+				if len(args) == 0 {
+					cmd.Help()
+					ctx.Exit(1)
+				}
+				selectedPod = args[0]
+			}
+			if deletePodConfig.Force || activekit.YesNo("Are you sure you want to delete pod %q in namespace %q?", selectedPod, ctx.GetNamespace()) {
+				if err := ctx.Client.DeletePod(ctx.GetNamespace().ID, selectedPod); err != nil {
+					ferr.Println(err)
+					ctx.Exit(1)
+				}
+				fmt.Println("OK")
 			}
 		},
 	}
