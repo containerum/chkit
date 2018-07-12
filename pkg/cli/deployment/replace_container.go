abc4191181153f884c56030c0caaf0e1a62aebb6
Ilia Donchenko <iliad@me.com>
2018-07-12 11:21:13 +0300

    Fix replace container

--- pkg/cli/deployment/replace_container.go
+++ pkg/cli/deployment/replace_container.go
@@ -60,12 +60,20 @@ func ReplaceContainer(ctx *context.Context) *cobra.Command {
 						ctx.Exit(1)
 					}
 					cont = cont.Patch(importedCont)
+				} else {
+					flagCont, err := flags.Container()
+					if err != nil {
+						ferr.Println(err)
+						ctx.Exit(1)
+					}
+					cont = cont.Patch(flagCont)
 				}
 				cont, err = flags.Patch(cont)
 				if err != nil {
 					ferr.Println(err)
 					ctx.Exit(1)
 				}
+				cont.Name = flags.ContainerName
 				depl.Containers, _ = depl.Containers.Replace(cont)
 				if err := ctx.Client.ReplaceDeployment(ctx.GetNamespace().ID, depl); err != nil {
 					ferr.Println(err)
@@ -146,6 +154,13 @@ func ReplaceContainer(ctx *context.Context) *cobra.Command {
 					ctx.Exit(1)
 				}
 				cont = cont.Patch(importedCont)
+			} else {
+				flagCont, err := flags.Container()
+				if err != nil {
+					ferr.Println(err)
+					ctx.Exit(1)
+				}
+				cont = cont.Patch(flagCont)
 			}
 
 			logger.Debugf("building container from flags")
